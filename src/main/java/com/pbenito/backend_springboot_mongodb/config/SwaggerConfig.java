605ed9d807de53c88beb4ea0f7b158e0600b44c3
barqueros2001 <ginesmartinezros@gmail.com>
2024-11-26 19:35:21 +0100

    Add Swagger dependencies

---
+++ src/main/java/com/pbenito/backend_springboot_mongodb/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.pbenito.backend_springboot_mongodb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // Define un esquema de seguridad para el encabezado "Authorization"
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Transactions API")
+                        .version("1.0.0")
+                        .description("API para gestionar transacciones"))
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("Authorization", securityScheme));
+    }
+}
